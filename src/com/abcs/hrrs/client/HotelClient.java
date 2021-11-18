618b4be76c468c6aca40055451f82fc52736f946
BakungaBronson <bakungabro@gmail.com>
2021-11-18 18:52:34 +0300

    Takes in arguments now and resolved the address issue

--- src/com/abcs/hrrs/client/HotelClient.java
+++ src/com/abcs/hrrs/client/HotelClient.java
@@ -1,13 +1,13 @@
-/**
- * 
- */
 package com.abcs.hrrs.client;
 
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import com.abcs.hrrs.data.Data;
 import com.abcs.hrrs.services.RoomManager;
 
 /**
@@ -19,31 +19,56 @@ import com.abcs.hrrs.services.RoomManager;
 public class HotelClient {
 
 	/**
-	 * @param args
+	 * Takes a string of arguments
 	 */
 	public static void main(String[] args) {
 		try {
 
-			// Create an instance
-			// RoomManager manager = (RoomManager)
-			// Naming.lookup("rmi://localhost:1099/rooms");
+			System.out.println("Connect : OK\n");
 
-			// manager.revenue();
+			if (args.length > 0) {
+				if (args[0].equals("list")) {
+					//rm.List();
 
-			Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+				} else if (args[0].equals("book")) {
+					//rm.Book(arg[1], arg[2]);
+					RoomManager manager = (RoomManager) Naming.lookup("rmi://"+args[1]+":1099/roomManager");
 
-			System.out.println("Connected 11 : \n");
+					try {
+						int val = Integer.parseInt(args[2]);
+						System.out.println(manager.book(val, args[3]));
+					}
+					catch (NumberFormatException e) {
 
-			RoomManager manager = (RoomManager) registry.lookup("rmi://localhost:1099/rooms");
+						// This is thrown when the String
+						// contains characters other than digits
+						System.out.println("Invalid number!");
+					}
 
-			System.out.println("Connected 112 : \n");
+				} else if (args[0].equals("guests")) {
 
-			displayRooms(manager.getRooms());
+					RoomManager manager = (RoomManager) Naming.lookup("rmi://"+args[1]+":1099/roomManager");
 
-			manager.revenue();
+					ArrayList<Data> res = manager.Guests();
+
+					int i = 0;
+					while (i < res.size()) {
+						System.out.print("Guest "+(i+1)+" name: " + res.get(i).guest + "\n");
+						i++;
+					}
+
+				} else if (args[0].equals("revenue")) {
+					RoomManager manager = (RoomManager) Naming.lookup("rmi://"+args[1]+":1099/roomManager");
+
+					System.out.println(manager.revenue());
+				}
+
+			} else {
+				System.out.println("print how to use");
+			}
 
 		} catch (Exception e) {
-			System.out.println("Recieved Exception : " + e);
+			System.out.println("Received Exception : " + e);
 
 		}
 
